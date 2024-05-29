5daa1ebceb85fad6428dca64407f0138d9d0dd50
do3 <bousnina.douaa1@gmail.com>
2024-05-29 20:51:33 +0100

    order table admin

--- src/Controller/AdminControllers/OrderAdminController.php
+++ src/Controller/AdminControllers/OrderAdminController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\AdminControllers;
 
+use App\Form\OrderType;
 use App\Repository\OrderRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,21 +23,33 @@ class OrderAdminController extends AbstractController
     }
 
     #[Route('/editOrder/{id}', name: 'app_edit_order_admin')]
-    public function edit(OrderRepository $orderRepository, int $id): Response
+    public function edit(OrderRepository $orderRepository, int $id, Request $request): Response
     {
         $order = $orderRepository->find($id);
+        $form = $this->createForm(OrderType::class, $order);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $order->setOrderTotalAmount($form->get('order_total_amount')->getData());
+            $order->setOrderDate($form->get('order_date')->getData());
+        
+            $this->addFlash('success', 'Order updated successfully!');
+
+            return $this->redirect('/adminOrder');
+        }
+
         return $this->render('AdminDashboard/editOrder.html.twig', [
-            'order' => $order
+            'form' => $form
         ]);
     }
-
-    
 
     #[Route('/deleteOrder/{id}', name: 'app_delete_order_admin')]
     public function delete(OrderRepository $orderRepository, EntityManager $entityManager, int $id): Response
     {
         $order = $orderRepository->find($id);
         $entityManager->remove($order);
+        $entityManager->persist($order);
         $entityManager->flush();
         return $this->redirect('/adminOrder');
     }
